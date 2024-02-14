428727719b4514dcb7ce60b7538e30aab90b0f63
Beeno Tung <aabbcc1241@yahoo.com.hk>
2024-02-14 19:11:55 +0800

    init: setup project from "npm init ts-liveview" with v5-auth-template

---
+++ client/internal.ts
@@ -0,0 +1,14 @@
+export type WindowStub = {
+  emit(...args: unknown[]): void
+  emitHref(event: Event, flag?: LinkFlag): void
+  emitForm(event: Event): void
+  submitForm(form: HTMLFormElement): void
+  get(url: string): Promise<Response>
+  del(url: string): Promise<Response>
+  upload(event: Event): Promise<Response>
+  remount(): void
+  _navigation_type_: 'static' | 'express' | 'ws'
+  _navigation_method_: 'GET' | 'POST' | 'PUT' | 'PATCH' | 'DELETE'
+}
+
+export type LinkFlag = ['q', 'f', 'b']
